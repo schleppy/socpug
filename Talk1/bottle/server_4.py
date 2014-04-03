49d6c76ed9520490dbd8803c213c54aaeb59f335
Sean Berry <sean@crowdstrike.com>
2014-04-03 10:20:50 -0700

    Updated example servers, removing crufy from previous versions.

--- Talk1/bottle/server_4.py
+++ Talk1/bottle/server_4.py
@@ -1,28 +1,13 @@
 import bottle
 from werkzeug.debug import DebuggedApplication
+from middleware import StripPathMiddleware
 
-class StripPathMiddleware(object):
-  def __init__(self, app):
-    self.app = app
-  def __call__(self, e, h):
-    e['PATH_INFO'] = e['PATH_INFO'].rstrip('/')
-    return self.app(e,h)
-
-@bottle.route("/")
-def hello():
-    return "Hello World!"
-
-@bottle.route("/hello")
-@bottle.route("/hello/<name>")
-def hello_name(name="World"):
-    return "Hello {}".format(name)
 
 @bottle.route("/fail")
 def fail():
     user = bottle.request.params.get("name")
     some_data = {"sean": "admin", "joe": "user"}
-    #return "{} is of role {}".format(user, some_data[user])
-    return "{} is of role {}".format(user, some_data.get(user, "no role"))
+    return "{} is of role {}".format(user, some_data[user])
 
 if __name__ == "__main__":
     app = bottle.app()
--- Talk1/bottle/server_5.py
+++ Talk1/bottle/server_5.py
@@ -3,12 +3,15 @@ import time
 
 @bottle.route("/beer")
 def hello():
+    bottle.response.headers['Content-Type'] = 'text/plain'
     for i in range(100, 0, -1):
         if i < 100:
-            yield "Take one down, pass it around, {} bottles of beer on the wall\n".format(i)
-            time.sleep(2)
-        yield "{} bottles of beer on the wall, {} bottles of beer\n".format(i, i)
+            yield "Take one down, pass it around, " \
+                   "{} bottles of beer on the wall\n".format(i)
+            time.sleep(.2)
+        yield "{} bottles of beer on the wall, " \
+               "{} bottles of beer\n".format(i, i)
 
 
 if __name__ == "__main__":
-    bottle.run()
+    bottle.run(debug=True)
